146587c66d832ed2d6b01f6abc8725acfb8f946a
marshallnaito <marshalln3@yahoo.com>
2015-08-10 09:16:09 -0700

    added the html/php form files

---
+++ miniprojectecho.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+		Results:<br><br>
+		First Name <?php echo $_POST["firstName"]; ?><br>
+		Last Name <?php echo $_POST["lastName"]; ?><br>
+		Email: <?php echo $_POST["email"]; ?>
+	</body>
+</html>
