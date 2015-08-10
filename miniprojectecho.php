f57165fc27a518e0f1aa0907c8a63d47dbe3c434
marshallnaito <marshalln3@yahoo.com>
2015-08-10 10:36:16 -0700

    deleted wrong file

--- miniprojectecho.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<body>
-		Results:<br><br>
-		First Name: <?php echo $_POST["firstName"]; ?><br>
-		Last Name: <?php echo $_POST["lastName"]; ?><br>
-		Email: <?php echo $_POST["email"]; ?>
-	</body>
-</html>
